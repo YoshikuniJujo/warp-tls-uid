58923dc627c7afa19c283990246cb3af4b7fc12b
Yoshikuni Jujo <PAF011432[at]nifty.ne.jp>
2018-05-26 12:30:17 +0900

    test suid

--- warp-tls-uid/app/Main.hs
+++ warp-tls-uid/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
 import Network.Wai
@@ -7,11 +9,37 @@ import Blaze.ByteString.Builder.Char.Utf8
 
 import Network.Wai.Handler.WarpTLS
 
+import qualified Data.ByteString as BS
+import Network.Socket
+import Control.Exception (bracket)
+import Data.Streaming.Network (bindPortTCP)
+import System.Posix
+
 server :: Application
 server _ f = f . responseBuilder status200 [] $ fromString "hello"
 
 main :: IO ()
-main = runTLS
-	(tlsSettings "first_cert.pem" "first_key_ngrd.pem")
+main = runTlsWithId
+	("first_cert.pem", "first_key_ngrd.pem")
+	("tatsuya", "tatsuya")
 	(setPort 8080 defaultSettings)
 	server
+
+runTlsWithId ::
+	(FilePath, FilePath) -> (Group, Usr) -> Settings -> Application -> IO ()
+runTlsWithId (crt, key) (g, u) set app = do
+	!c <- BS.readFile crt
+	!k <- BS.readFile key
+	let	tset = tlsSettingsMemory c k
+	withSocketsDo $ bracket
+		(bindPortTCPWithId (g, u) (getPort set) (getHost set))
+		close
+		(\sock -> runTLSSocket tset set sock app)
+
+type Group = String
+type Usr = String
+
+bindPortTCPWithId :: (Group, Usr) -> Int -> HostPreference -> IO Socket
+bindPortTCPWithId (g, u) p h = (bindPortTCP p h <*) $ do
+	getGroupEntryForName g >>= setGroupID . groupID
+	getUserEntryForName u >>= setUserID . userID
